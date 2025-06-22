84cedbe98fdf7a01b433bd25b1ab926e16ac3926
5hojib <yesiamshojib@gmail.com>
2025-06-22 11:00:09 +0000

    format: auto-format code by ruff.
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- update.py
+++ update.py
@@ -1,34 +1,29 @@
-from dotenv import (
-    load_dotenv,
-    dotenv_values
-)
 from logging import (
     ERROR,
     INFO,
-    basicConfig,
-    error as log_error,
     FileHandler,
-    getLogger,
-    info as log_info,
     StreamHandler,
+    basicConfig,
+    getLogger,
 )
-from os import (
-    environ,
-    path,
-    remove
+from logging import (
+    error as log_error,
 )
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from logging import (
+    info as log_info,
+)
+from os import environ, path, remove
 from subprocess import run as urun
 from sys import exit
+
+from dotenv import dotenv_values, load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 getLogger("pymongo").setLevel(ERROR)
 
 if path.exists("Zee_Logs.txt"):
-    with open(
-        "Zee_Logs.txt",
-        "r+"
-    ) as f:
+    with open("Zee_Logs.txt", "r+") as f:
         f.truncate(0)
 
 if path.exists("rlog.txt"):
@@ -36,17 +31,11 @@ if path.exists("rlog.txt"):
 
 basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        FileHandler("Zee_Logs.txt"),
-        StreamHandler()
-    ],
+    handlers=[FileHandler("Zee_Logs.txt"), StreamHandler()],
     level=INFO,
 )
 
-load_dotenv(
-    "config.py",
-    override=True
-)
+load_dotenv("config.py", override=True)
 
 try:
     if bool(environ.get("_____REMOVE_THIS_LINE_____")):
@@ -55,40 +44,30 @@ try:
 except:
     pass
 
-BOT_TOKEN = environ.get(
-    "BOT_TOKEN",
-    ""
-)
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
-BOT_ID = BOT_TOKEN.split(
-    ":",
-    1
-)[0]
+BOT_ID = BOT_TOKEN.split(":", 1)[0]
 
-DATABASE_URL = environ.get(
-    "DATABASE_URL",
-    ""
-)
+DATABASE_URL = environ.get("DATABASE_URL", "")
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
 if DATABASE_URL is not None:
     try:
-        conn = MongoClient(
-            DATABASE_URL,
-            server_api=ServerApi("1")
-        )
+        conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
         db = conn.zee
         old_config = db.settings.deployConfig.find_one({"_id": BOT_ID})
         config_dict = db.settings.config.find_one({"_id": BOT_ID})
         if old_config is not None:
             del old_config["_id"]
         if (
-            old_config is not None
-            and old_config == dict(dotenv_values("config.env"))
+            (
+                old_config is not None
+                and old_config == dict(dotenv_values("config.env"))
+            )
             or old_config is None
         ) and config_dict is not None:
             environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
@@ -97,27 +76,17 @@ if DATABASE_URL is not None:
     except Exception as e:
         log_error(f"Database ERROR: {e}")
 
-UPSTREAM_REPO = environ.get(
-    "UPSTREAM_REPO",
-    ""
-)
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
 if len(UPSTREAM_REPO) == 0:
     UPSTREAM_REPO = "https://github.com/jattpbx/aeon-extended"
 
-UPSTREAM_BRANCH = environ.get(
-    "UPSTREAM_BRANCH",
-    ""
-)
+UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
 if len(UPSTREAM_BRANCH) == 0:
     UPSTREAM_BRANCH = "main"
 
 if UPSTREAM_REPO is not None:
     if path.exists(".git"):
-        urun([
-            "rm",
-            "-rf",
-            ".git"
-        ])
+        urun(["rm", "-rf", ".git"], check=False)
 
     update = urun(
         [
@@ -130,6 +99,7 @@ if UPSTREAM_REPO is not None:
                      && git fetch origin -q \
                      && git reset --hard origin/{UPSTREAM_BRANCH} -q"
         ],
+        check=False,
         shell=True,
     )
 
@@ -149,6 +119,7 @@ urun(
         "Dockerfile",
         "LICENSE",
         "README.md",
-        "requirements.txt"
-    ]
+        "requirements.txt",
+    ],
+    check=False,
 )
